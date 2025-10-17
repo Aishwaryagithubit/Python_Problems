ce6c8bce3cb74f5e7ab20cb3af0bce75b98a9cec
Aishwarya Sah <146162125+Aishwaryagithubit@users.noreply.github.com>
2025-10-17 23:26:56 +0545

    Fix in as10.0.py

--- as10.0.py
+++ as10.0.py
@@ -1,7 +1,8 @@
 def user(input_list):
     return {name: number for name, number in input_list}
 
-input_list = [("Dan Mace", 98980000), ("Connor Meyer", 777890879), ("Adam Smith", 123087699), ("John Watson", 960008779)]
+input_list = [("Dan Mace", 98980000), ("Connor Meyer", 077890879), ("Adam Smith", 123087699), ("John Watson", 960008779)]
 
 output_dict = user(input_list)
 print(output_dict)
+
