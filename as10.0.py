3c5b38ba2b1873a3289968e028eb4f669ea29878
Aishwarya Sah <146162125+Aishwaryagithubit@users.noreply.github.com>
2026-01-23 22:47:57 +0545

    Update as10.0.py

--- as10.0.py
+++ as10.0.py
@@ -1,10 +1,11 @@
 def user(input_list):
     return {name: number for name, number in input_list}
 
-input_list = [("Dan Mace", 9898000), ("Connor Meyer", 077890879), ("Adam Smith", 123087699), ("John Watson", 960008789)]
+input_list = [("Dan Mace", 98980000), ("Connor Meyer", 077890879), ("Adam Smith", 123087699), ("John Watson", 960008789)]
 
 output_dict = user(input_list)
 print(output_dict)
+
 
 
 
