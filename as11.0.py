67bf2eb6da90450c361e38441698eda0b174733c
Aishwarya Sah <146162125+Aishwaryagithubit@users.noreply.github.com>
2026-05-04 22:32:42 +0545

    Update as11.0.py

--- as11.0.py
+++ as11.0.py
@@ -3,5 +3,5 @@ def words(input_str):
 
 input_str = "If you count the words in this sentence you will get twelve"
 num_words = words(input_str)
-print("Number of words:",num_words)
+print("Number of words:", num_words)
 
